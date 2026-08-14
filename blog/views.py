e376962bf9537038059f818afd8647830c1ef842
matin salehi <symatin3@gmail.com>
2026-08-14 21:01:43 +0330

    fix(drf): use request.data for POST data

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,9 @@ from rest_framework.decorators import api_view
 @api_view(['GET', 'POST'])
 def index(request):
     if request.method == 'POST':
-        name = request.GET.get('name')
-        lastname = request.GET.get('lastname')
-        return Response({'message': f'Hello {name} {lastname}'})
+        name = request.data.get('name')
+        lastname = request.data.get('lastname')
+        return Response({'message': f'HELLO {name} {lastname}'})
     if request.method == 'GET':
         name = request.GET.get("name")
         return Response({'name': name})
@@ -20,6 +20,5 @@ class IndexView(APIView):
         name = request.GET.get('name')
         return Response({'message': f'Hello {name}'})
     def post(self, request):
-        name = request.GET.get('name')
-        lastname = request.GET.get('lastname')
-        return Response({'message': f'Hello {name} {lastname}'})
+        data = request.data
+        return Response({'message': f'Hello {data['name']} {data['lastname']}'})
